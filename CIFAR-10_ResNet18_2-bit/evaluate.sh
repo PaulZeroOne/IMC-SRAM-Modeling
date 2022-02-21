7f0c9ea73babd95288875b508ee7cef7704d4f6c
sharmaa4 <40257066+sharmaa4@users.noreply.github.com>
2022-02-21 12:25:15 +0530

    evaluate.sh file updated with some parameters given for pipe cleaning

--- CIFAR-10_ResNet18_2-bit/evaluate.sh
+++ CIFAR-10_ResNet18_2-bit/evaluate.sh
@@ -1,10 +1,6 @@
 ############ directory to save result #############
-
-if [ ! -d "$DIRECTORY" ]; then
-    mkdir ./save
-    mkdir ./dataset
-fi
-
+mkdir ./save
+mkdir ./dataset
 model=resnet18_quant_tanh
 dataset=cifar10
 epochs=10
@@ -19,33 +15,8 @@ ratio=0.7
 wd=0.0005
 lr=0.1
 #q_file=$4
-
-save_path="./save/${model}/${model}_w${wbit}_a${abit}_mode_${mode}_k${k}_lambda${ub}_wd${wd}_swpFalse_g02_Eval/"
-log_file="${model}_w${wbit}_a${abit}_mode${mode}_k${k}_lambda${ub}_wd${wd}_swpFalse_Eval.log"
+save_path="./save/resnet18_quant_tanh/resnet18_quant_tanh_w2_a2_mode_mean_k2_lambda_wd0.0005_swpFalse_g02/"
+log_file="resnet18_quant_tanh_w2_a2_mode_mean_k2_lambda_wd0.0005_swpFalse_g02.log"
 pretrained_model="./save/resnet18_quant_tanh/resnet18_quant_tanh_w2_a2_mode_mean_k2_lambda_wd0.0005_swpFalse_g02/model_best.pth.tar"
-
-CUDA_LAUNCH_BLOCKING=1 python -W ignore train.py --dataset ${dataset} \
-    --data_path ./dataset/ \
-    --model ${model} \
-    --save_path ${save_path} \
-    --epochs ${epochs} \
-    --log_file ${log_file} \
-    --lr  ${lr} \
-    --schedule 60 120 \
-    --gammas 0.1 0.1 \
-    --batch_size ${batch_size} \
-    --ngpu 1 \
-    --wd ${wd} \
-    --k ${k} \
-    --group_ch ${group_ch} \
-    --wbit ${wbit} \
-    --abit ${abit} \
-    --resume ${pretrained_model} \
-    --fine_tune \
-    --eval \
-    --offset_noise $2 \
-    --bitline_noise $3 \
-    --sram_depth $1 \
-    --quant_bound $4 \
-    --number_levels $5
-    
+CUDA_LAUNCH_BLOCKING=1 python -W ignore train.py --dataset cifar10 --data_path ./dataset/     --model resnet18_quant_tanh     --save_path "./save/resnet18_quant_tanh/resnet18_quant_tanh_w2_a2_mode_mean_k2_lambda_wd0.0005_swpFalse_g02/"     --epochs ${epochs}     --log_file  "resnet18_quant_tanh_w2_a2_mode_mean_k2_lambda_wd0.0005_swpFalse_g02.log"    --lr  ${lr}     --schedule 60 120     --gammas 0.1 0.1     --batch_size ${batch_size}     --ngpu 1     --wd ${wd}     --k ${k}     --group_ch ${group_ch}     --wbit ${wbit}     --abit ${abit} --quant_bound 1.0 --number_levels 2 --offset_noise 2.5 --bitline_noise 6 --sram_depth 256 --finetune
+#--resume  "./save/resnet18_quant_tanh/resnet18_quant_tanh_w2_a2_mode_mean_k2_lambda_wd0.0005_swpFalse_g02/model_best.pth.tar"    --fine_tune
